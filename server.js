4f27b8866834bee7b0d3904643525152e715e8c1
Nome Sobrenome <rafaelconquista2002@hotmail.com>
2024-02-05 22:50:29 -0300

    testes de deploy

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import app from './app.js';
 const port = 3005 //modificar para uso de DOTENV
 
 app.listen(port, () => {
+    console.log(app)
     console.log(`escutando na porta ${port}`);
     console.log(`CTRL + clique em http://localhost:${port}`);
 });
